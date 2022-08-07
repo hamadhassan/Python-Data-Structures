9e77485cec117a3e1f41b5d40d6a75df27a156ac
Muhammad Hammad Hassan <mhhjutt@gmail.com>
2022-08-07 16:40:24 +0500

    practice the strings

--- strings.py
+++ strings.py
@@ -1,1 +1,19 @@
-print("Hello World")
+str1 = "Hello"
+str2 = 'there'
+bob = str1 + str2
+print(bob)
+x = '40'
+y = int(x) + 2
+print(y)
+x = 'From marquard@uct.ac.za'
+print(x[8])
+x = 'From marquard@uct.ac.za'
+print(x[15:3])
+print(x[14:17])
+print(len('banana')*7)
+greet = 'Hello Bob'
+print(greet.upper())
+print(greet.split())
+data = 'From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008'
+pos = data.find('.')
+print(data[pos:pos+3])
